9856c077e2cadae84e7e6fbb1f47d91ac15733a4
Alexander <makedonski_rulez@mail.ru>
2024-10-13 18:52:40 +0300

    training method filter --- fault!!!

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,61 @@
 import React, {useState} from 'react';
-import {ArrayCars, NewComponent} from './components/NewComponent';
+import {NewComponent} from './site/Test';
 
-function App() {
+type nameButtonType =  'alls' | 'rubles' | 'dollars'
 
-    const topCars: ArrayCars[] = [
-        {manufacturer: 'BMW', model: 'X5'},
-        {manufacturer: 'Mersedes', model: 'MLS'},
-        {manufacturer: 'Audi', model: 'Q7'}
-    ]
+
+export function App() {
+
+    const [money, setMoney] = useState([
+        {banknote: 'dollar', nominal: 100, number: 'a123456789'},
+        {banknote: 'dollar', nominal: 50, number: 'b123456789'},
+        {banknote: 'ruble', nominal: 100, number: 'c123456789'},
+        {banknote: 'dollar', nominal: 100, number: 'd123456789'},
+        {banknote: 'dollar', nominal: 50, number: 'e123456789'},
+        {banknote: 'ruble', nominal: 100, number: 'f123456789'},
+        {banknote: 'dollar', nominal: 50, number: 'j123456789'},
+        {banknote: 'ruble', nominal: 50, number: 'h123456789'}
+    ])
+
+    const [nameButton, SetNameButton] = useState<nameButtonType>('alls')
+
+    let currentMoney = money
+
+    if (nameButton === 'rubles') {
+        currentMoney = money.filter((el) =>
+            el.banknote === 'ruble'
+        )
+    }
+    if (nameButton === 'dollars') {
+        currentMoney = money.filter((el) =>
+            el.banknote === 'dollar'
+        )
+    }
+
+    const onClickFilterHandler = (currency: nameButtonType) => {
+        SetNameButton(currency)
+    }
 
     return (
-        <NewComponent cars={topCars}/>
+        <>
+            <NewComponent />
+{/*            <ul>
+                {currentMoney.map((m, index) => {
+                    return (
+                        <li key={index} style={{listStyle: 'none'}}>
+                            <span>{m.banknote}</span>
+                            <span>{m.nominal}</span>
+                            <span>{m.number}</span>
+                        </li>
+                    )
+                })}
+            </ul>
+            <div style={{marginLeft: '35px'}}>
+                <button onClick={() => onClickFilterHandler('alls')}>all</button>
+                <button onClick={() => onClickFilterHandler('rubles')}>ruble</button>
+                <button onClick={() => onClickFilterHandler('dollars')}>dollar</button>
+            </div>*/}
+        </>
     );
 }
 
