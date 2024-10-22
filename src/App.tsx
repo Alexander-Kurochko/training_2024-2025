644b43542d9c01d87c480f3f19daa842c7d3a2fc
Alexander <makedonski_rulez@mail.ru>
2024-10-22 22:50:06 +0300

    training Input and Button

--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,39 @@
 import React, {useState} from 'react';
-import {NewComponent} from './site/Test';
-
-type nameButtonType =  'alls' | 'rubles' | 'dollars'
+import {FullInput} from './components/FullInpunt';
+import {Input} from './components/Input';
+import {Button} from './components/Button';
 
 
 export function App() {
+    const [message, setMessage] = useState([
+            {message: 'message1'},
+            {message: 'message2'},
+            {message: 'message3'}
+        ]
+    )
 
-    const [money, setMoney] = useState([
-        {banknote: 'dollar', nominal: 100, number: 'a123456789'},
-        {banknote: 'dollar', nominal: 50, number: 'b123456789'},
-        {banknote: 'ruble', nominal: 100, number: 'c123456789'},
-        {banknote: 'dollar', nominal: 100, number: 'd123456789'},
-        {banknote: 'dollar', nominal: 50, number: 'e123456789'},
-        {banknote: 'ruble', nominal: 100, number: 'f123456789'},
-        {banknote: 'dollar', nominal: 50, number: 'j123456789'},
-        {banknote: 'ruble', nominal: 50, number: 'h123456789'}
-    ])
+    let [title, setTitle] = useState('')
 
-    const [nameButton, SetNameButton] = useState<nameButtonType>('alls')
-
-    let currentMoney = money
-
-    if (nameButton === 'rubles') {
-        currentMoney = money.filter((el) =>
-            el.banknote === 'ruble'
-        )
-    }
-    if (nameButton === 'dollars') {
-        currentMoney = money.filter((el) =>
-            el.banknote === 'dollar'
-        )
+    const addMessage = (title: string) => {
+        let newMessage = {message: title}
+        setMessage([newMessage, ...message])
     }
 
-    const onClickFilterHandler = (currency: nameButtonType) => {
-        SetNameButton(currency)
+    const callBackButtonHandler = () => {
+        addMessage(title)
+        setTitle('')
     }
 
     return (
-        <>
-            <NewComponent />
-{/*            <ul>
-                {currentMoney.map((m, index) => {
-                    return (
-                        <li key={index} style={{listStyle: 'none'}}>
-                            <span>{m.banknote}</span>
-                            <span>{m.nominal}</span>
-                            <span>{m.number}</span>
-                        </li>
-                    )
-                })}
-            </ul>
-            <div style={{marginLeft: '35px'}}>
-                <button onClick={() => onClickFilterHandler('alls')}>all</button>
-                <button onClick={() => onClickFilterHandler('rubles')}>ruble</button>
-                <button onClick={() => onClickFilterHandler('dollars')}>dollar</button>
-            </div>*/}
-        </>
+        <div className="App">
+            {/*            <FullInput addMessage={addMessage}/>*/}
+            <Input setTitle={setTitle} title={title}/>
+            <Button name={'+'} callBack={callBackButtonHandler}/>
+            {message.map((el, index) => {
+                return (
+                    <div key={index}>{el.message}</div>
+                )
+            })}
+        </div>
     );
 }
-
-export default App;
